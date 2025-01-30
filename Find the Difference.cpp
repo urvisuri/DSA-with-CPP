ca90bbe50a6c67b0eca10e2b6356c4151896c558
URVI SURI <suriurvi888@gmail.com>
2025-01-30 20:25:11 +0530

    File Updated

--- Find the Difference.cpp
+++ Find the Difference.cpp
@@ -4,7 +4,7 @@ public:
          char result = 0; // Initialize with 0 since XOR with 0 doesn't change the value
         for (char c : s) {
             result ^= c; // XOR all characters in s
-        }
+        }   
         for (char c : t) {
             result ^= c; // XOR all characters in t
         }
