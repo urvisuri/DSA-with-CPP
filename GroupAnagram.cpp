66eb10c88d6fdb15460edf3fc91ace4686fd7969
URVI SURI <suriurvi888@gmail.com>
2025-02-03 21:51:14 +0530

    File Updated

--- GroupAnagram.cpp
+++ GroupAnagram.cpp
@@ -9,8 +9,6 @@ public:
             std::sort(sortedStr.begin(), sortedStr.end()); // Sort to get unique key
             anagramGroups[sortedStr].push_back(str);
         }
-
-        // Convert map values to a vector of vectors
         std::vector<std::vector<std::string>> result;
         for (const auto& group : anagramGroups) {
             result.push_back(group.second);
