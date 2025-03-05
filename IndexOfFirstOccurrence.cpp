b60b7e4999b0e5018ef9c4d0e8bf5b2ad59ee6dc
URVI SURI <suriurvi888@gmail.com>
2025-03-05 23:49:17 +0530

    File Updated

--- IndexOfFirstOccurrence.cpp
+++ IndexOfFirstOccurrence.cpp
@@ -3,11 +3,11 @@ public:
     int strStr(string haystack, string needle) {
         if (needle.empty()) return 0;
 
-        // Ensure that the haystack has enough space to contain the needle
+        
         int n = haystack.size(), m = needle.size();
         if (m > n) return -1; // If needle is longer than haystack, return -1
 
-        // Loop through the haystack, ensuring we don't go out of bounds
+        
         for (int i = 0; i <= n - m; ++i) {
             // Compare the substring with the needle
             if (haystack.substr(i, m) == needle) {
